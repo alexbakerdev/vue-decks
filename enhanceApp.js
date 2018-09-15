d025b4baf3c08e2d75191ef4fc63a64a7b56d4c5
Alex Baker <alex@connected-ventures.com>
2018-09-15 22:28:36 +0100

    rewrite list animation component

--- enhanceApp.js
+++ enhanceApp.js
@@ -14,21 +14,69 @@ export default ({ Vue }) => {
       if (context.children) {
         if (context.children.length === 1) {
           const rootChild = context.children[0];
-          const listItems = rootChild.children;
+          const listItems = rootChild.children || [];
           return [
             h(
               "TransitionGroup",
               {
                 props: {
                   name: "list",
-                  tag: rootChild.tag
+                  tag: "div"
+                },
+                class: {
+                  "transition-list": true
                 }
               },
               listItems.reduce((children, li, i) => {
                 if (li.tag) {
                   li.data = li.data || {};
+                  li.data.style = { display: "block" };
                   li.data.attrs = li.data.attrs || {};
                   li.data.key = `list-transition-${i}`;
+                  li.data.class = li.data.class || {};
+                  li.data.class["list-item"] = true;
+                  li = h("li", li.data, li.children);
+                }
+
+                children.push(li);
+                return children;
+              }, [])
+            )
+          ];
+        }
+      }
+    }
+  });
+
+  Vue.component("MagicTransition", {
+    name: "MagicTransition",
+    functional: true,
+    render(h, context) {
+      if (context.children) {
+        if (context.children.length === 1) {
+          const rootChild = context;
+          const listItems = rootChild.children || [];
+          return [
+            h(
+              "TransitionGroup",
+              {
+                props: {
+                  name: "list",
+                  tag: "div"
+                },
+                class: {
+                  "transition-list": true
+                }
+              },
+              listItems.reduce((children, li, i) => {
+                debugger;
+                if (li.tag) {
+                  li.data = li.data || {};
+                  li.data.style = { display: "block" };
+                  li.data.attrs = li.data.attrs || {};
+                  li.data.key = `list-transition-${i}`;
+                  li.data.class = li.data.class || {};
+                  li.data.class["list-item"] = true;
                   li = h(li.tag, li.data, li.children);
                 }
 
